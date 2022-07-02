ad2252c97b4b43cd42fd34c05f681a82e650254c
GhertaDan <deepdanny2003@gmail.com>
2022-07-02 18:23:18 +0300

    navbar class fix

--- notes/src/components/AppNavbar.vue
+++ notes/src/components/AppNavbar.vue
@@ -5,8 +5,8 @@
         <img src="../assets/images/navbar/Logo.png" alt="" />
       </div>
       <div class="navbar__auth">
-        <app-button class="navbar__login">Log in</app-button>
-        <app-button class="navbar__signin">Sign in</app-button>
+        <app-button class="navbar__login" :class="login">Log in</app-button>
+        <app-button class="navbar__signin" :class="signin">Sign in</app-button>
       </div>
     </div>
   </header>
@@ -17,21 +17,31 @@ import AppButton from "./UI/AppButton.vue";
 export default {
   name: "AppNavbar",
   components: { AppButton },
+  data() {
+    return {
+      login: "login",
+      signin: "signin",
+      gradient: "gradient",
+    };
+  },
 };
 </script>
 
 <style lang="scss">
-.navbar {
-  &__login {
-    border: 3px solid #cdcdcd;
-    border-radius: 8px;
-    padding: 5px 20px;
-  }
-  &__signin {
-    background-color: #cdcdcd;
-    border-radius: 8px;
-    padding: 5px 20px;
-    color: #fff;
-  }
+.login {
+  border: 3px solid #cdcdcd;
+  border-radius: 8px;
+  padding: 5px 20px;
+}
+.signin {
+  background-color: #cdcdcd;
+  border-radius: 8px;
+  padding: 5px 20px;
+  color: #fff;
+}
+.gradient {
+  background: linear-gradient(to right, #cdcdcd, #fff);
+  border-radius: 8px;
+  padding: 5px 20px;
 }
 </style>
