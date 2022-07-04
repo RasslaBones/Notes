a585656108c4b26727c43a972af233741df9c175
GhertaDan <deepdanny2003@gmail.com>
2022-07-04 14:24:16 +0300

    modal functional fix

--- notes/src/views/Home.vue
+++ notes/src/views/Home.vue
@@ -1,24 +1,29 @@
 <template>
-
   <div class="home__container home">
     <div class="home__buttons buttons">
       <div class="buttons__create">
-        <img src="@/assets/icons/plus.svg">
-        <div class="buttons__text">Create</div>
+        <img src="@/assets/icons/plus.svg" />
+        <div class="buttons__text" @click="openModal()">Create</div>
       </div>
       <div class="buttons__view view">
-
-        <div class="view__small" :class="isSmall ? 'active' : ''" @click="this.isSmall = true">
-            <img src="../assets/icons/list-check.svg" />
+        <div
+          class="view__small"
+          :class="isSmall ? 'active' : ''"
+          @click="isSmall = true"
+        >
+          <img src="../assets/icons/list-check.svg" />
         </div>
 
         <div class="view__line"></div>
 
-        <div class="view__large" :class="!isSmall ? 'active' : ''" @click="this.isSmall = false">
-            <img src="../assets/icons/list-check.svg" />
-            <img src="../assets/icons/list-check.svg" />
+        <div
+          class="view__large"
+          :class="!isSmall ? 'active' : ''"
+          @click="isSmall = false"
+        >
+          <img src="../assets/icons/list-check.svg" />
+          <img src="../assets/icons/list-check.svg" />
         </div>
-
       </div>
     </div>
     <div class="home__empty">
